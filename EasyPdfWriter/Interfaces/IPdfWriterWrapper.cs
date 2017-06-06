ddd521a0d6d3adff7e0756183479468ae7c1f3bd
Christoph Knobbe <christoph.knobbe2324@gmail.com>
2017-06-06 11:01:07 +0200

    Interface for dependency injection

---
+++ EasyPdfWriter/Interfaces/IPdfWriterWrapper.cs
@@ -0,0 +1,17 @@
+﻿namespace EasyPdfWriter.Interfaces
+{
+  /// <summary>
+  /// The PdfWriterWrapper Interface.
+  /// </summary>
+  public interface IPdfWriterWrapper
+  {
+    /// <summary>
+    ///   Fills the given pdf with the object.
+    /// </summary>
+    /// <param name="pdfToFillBytes">The pdf to fill.</param>
+    /// <param name="obj">The object to use.</param>
+    /// <param name="setReadOnly">Sets the pdf in a read only state.</param>
+    /// <returns>The filled pdf.</returns>
+    byte[] FillPdf(byte[] pdfToFillBytes, object obj, bool setReadOnly = true);
+  }
+}
