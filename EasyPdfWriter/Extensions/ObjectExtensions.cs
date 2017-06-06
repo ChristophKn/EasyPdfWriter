94df7f164cab0bdac8f264896d8b04264712b4a1
Christoph Knobbe <christoph.knobbe2324@gmail.com>
2017-06-06 11:00:56 +0200

    Changed namespace

---
+++ EasyPdfWriter/Extensions/ObjectExtensions.cs
@@ -0,0 +1,116 @@
+﻿namespace EasyPdfWriter.Extensions
+{
+  using System;
+  using System.IO;
+  using System.Linq;
+  using System.Reflection;
+  using System.Xml;
+  using iTextSharp.text.pdf;
+
+  /// <summary>
+  ///   The ObjectExtensions class.
+  /// </summary>
+  public static class ObjectExtensions
+  {
+    /// <summary>
+    ///   Fills the given pdf bytes with the given object.
+    /// </summary>
+    /// <param name="obj">The object to use.</param>
+    /// <param name="pdfToFillBytes">The pdf to fill bytes.</param>
+    /// <param name="setReadOnly">A value indicating whether to set the pdf read only or not.</param>
+    /// <returns>The filled pdf.</returns>
+    public static byte[] ToPdf(this object obj, byte[] pdfToFillBytes, bool setReadOnly = true)
+    {
+      if (obj.IsNull())
+      {
+        throw new ArgumentNullException(nameof(obj), "The obj cannot be null.");
+      }
+
+      if (!pdfToFillBytes.Any())
+      {
+        throw new ArgumentNullException(nameof(pdfToFillBytes), "Pdf to fill bytes cannot be null or empty.");
+      }
+
+      using (var sourcePdf = new PdfReader(pdfToFillBytes))
+      using (var destinationStream = new MemoryStream())
+      using (var destinationPdf = new PdfStamper(sourcePdf, destinationStream))
+      {
+        destinationPdf.SetFields(obj);
+
+        if (setReadOnly)
+        {
+          destinationPdf.SetReadOnly();
+        }
+
+        destinationPdf.Close();
+
+        return destinationStream.ToArray();
+      }
+    }
+
+    /// <summary>
+    ///   Converts the object to xfa compliant xml.
+    /// </summary>
+    /// <param name="obj">The object to convert.</param>
+    /// <returns>The converted xml node.</returns>
+    public static XmlNode ToXfaCompliantXml(this object obj)
+    {
+      var doc = new XmlDocument();
+
+      var root = doc.AppendChild(doc.CreateElement("root"));
+      root.AppendChild(obj);
+
+      return root;
+    }
+
+    /// <summary>
+    ///   Gets the property value with the given name.
+    /// </summary>
+    /// <typeparam name="T">The type of the value to get.</typeparam>
+    /// <param name="obj">The object to use.</param>
+    /// <param name="propertyName">The property name to use.</param>
+    /// <returns>The value of the property or the default value of the given type.</returns>
+    public static T GetPropertyValue<T>(this object obj, string propertyName)
+    {
+      if (obj.IsNull())
+      {
+        return default(T);
+      }
+
+      var property = obj.GetType()
+        .GetProperty(propertyName, BindingFlags.IgnoreCase | BindingFlags.Instance | BindingFlags.Public | BindingFlags.NonPublic);
+
+      return property.IsNotNull() ? (T)property.GetValue(obj) : default(T);
+    }
+
+    /// <summary>
+    ///   Returns an empty string if the object is null or the object as a string if not null.
+    /// </summary>
+    /// <param name="obj">The object to convert.</param>
+    /// <returns>An empty string or the object as a string.</returns>
+    public static string ToStringSafe(this object obj)
+    {
+      return obj?.ToString() ?? string.Empty;
+    }
+
+    /// <summary>
+    ///   Checks if the given obj is null.
+    /// </summary>
+    /// <param name="obj">The object to check.</param>
+    /// <returns>A value indicating whether the object is null or not.</returns>
+    public static bool IsNull(this object obj)
+    {
+      return obj == null;
+    }
+
+    /// <summary>
+    /// Checks if the given obj is not null.
+    /// </summary>
+    /// <param name="obj">The object to check.</param>
+    /// <returns>A value indicating whether the object is not null or is null.</returns>
+    public static bool IsNotNull(this object obj)
+    {
+      return obj != null;
+    }
+  }
+}
